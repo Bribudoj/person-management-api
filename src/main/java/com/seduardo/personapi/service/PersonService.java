454ed17dec8dc0cba14292ba62d0d74f1c504b4a
eduardoms <eduardomsantana@outlook.com.br>
2021-09-08 18:54:13 -0300

    Improved person create functionality

--- src/main/java/com/seduardo/personapi/service/PersonService.java
+++ src/main/java/com/seduardo/personapi/service/PersonService.java
@@ -1,26 +1,31 @@
 package com.seduardo.personapi.service;
 
 import com.seduardo.personapi.Repository.PersonRepository;
+import com.seduardo.personapi.dto.request.PersonDTO;
 import com.seduardo.personapi.dto.response.MessageResponseDTO;
 import com.seduardo.personapi.entity.Person;
+import com.seduardo.personapi.mapper.PersonMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+
 
 @Service
 public class PersonService {
 
     private PersonRepository personRepository;
+    private final PersonMapper personMapper = PersonMapper.INSTANCE;
 
     @Autowired
     public PersonService(PersonRepository personRepository){
         this.personRepository = personRepository;
     }
 
-    public MessageResponseDTO createPerson(Person person){
+    public MessageResponseDTO createPerson(PersonDTO personDTO){
 
-        Person savedPerson = personRepository.save(person);
+        Person personToSave = personMapper.toModel(personDTO);
+
+        Person savedPerson = personRepository.save(personToSave);
+
         return MessageResponseDTO
                 .builder()
                 .message("Created person with ID" + savedPerson.getId())
